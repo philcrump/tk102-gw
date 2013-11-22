e78a080e597232a37499b22ff032ec76ff2dee51
Phil Crump <phildcrump@gmail.com>
2013-11-22 02:05:34 +0000

    Add battery status to map.

--- www/data.php
+++ www/data.php
@@ -24,10 +24,10 @@ $json_output[]=[$lfix, $lupd];
 $output = array();
 $row = array();
 if(isset($_REQUEST["since_id"])) {
-	$data_stmt = $dbc->prepare("SELECT id,time,latitude,longitude,speed,bearing,battery,sig,sats FROM nupositions WHERE id>?;");
+	$data_stmt = $dbc->prepare("SELECT id,time,latitude,longitude,speed,bearing,battery,battstatus,sig,sats FROM nupositions WHERE id>?;");
 	$data_stmt->bindValue(1, $_REQUEST["since_id"], PDO::PARAM_INT);
 } else {
-	$data_stmt = $dbc->prepare("SELECT id,time,latitude,longitude,speed,bearing,battery,sig,sats FROM nupositions;");
+	$data_stmt = $dbc->prepare("SELECT id,time,latitude,longitude,speed,bearing,battery,battstatus,sig,sats FROM nupositions;");
 }
 $data_stmt->execute();
 $data_stmt->bindColumn(1, $id);
@@ -37,8 +37,9 @@ $data_stmt->bindColumn(4, $lon);
 $data_stmt->bindColumn(5, $spd);
 $data_stmt->bindColumn(6, $trk);
 $data_stmt->bindColumn(7, $bat);
-$data_stmt->bindColumn(8, $sig);
-$data_stmt->bindColumn(9, $sat);
+$data_stmt->bindColumn(8, $bst);
+$data_stmt->bindColumn(9, $sig);
+$data_stmt->bindColumn(10, $sat);
 $output = array();
 while ($data_stmt->fetch()) {
     $row['id']=$id;
@@ -48,6 +49,7 @@ while ($data_stmt->fetch()) {
     $row['spd']=$spd;
     $row['trk']=$trk;
     $row['bat']=$bat;
+    $row['bst']=$bst;
     $row['sig']=$sig;
     $row['sat']=$sat;
     $output[]=$row;
